626756e6c2107d52c450399cbcff10c98f72be28
eimspl <marcin@eims.pl>
2020-07-07 23:18:18 +0200

    correct

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -0,0 +1,28 @@
+// 8.2
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if(b == 0){
+            throw new ArithmeticException("Remember, damn it, don't divide by zero");
+        }
+        return a / b;
+    }
+
+    /**
+     * This main can throw an ArithmeticException!!!
+     * @param args
+     */
+    public static void main(String[] args) {
+        FirstChallenge firstChallenge = new FirstChallenge();
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (ArithmeticException ex) {
+            System.out.println("Exeption: " + ex);
+        } finally {
+            System.out.println("Tray again");
+        }
+    }
+}
