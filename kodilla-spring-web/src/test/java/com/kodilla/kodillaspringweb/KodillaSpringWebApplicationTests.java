80ad043ca956be9a960c4948c89a796e8ed5fcc5
Marcin Łukasik <marcin@eims.pl>
2020-11-08 19:42:33 +0100

    update after repair

--- kodilla-spring-web/src/test/java/com/kodilla/kodillaspringweb/KodillaSpringWebApplicationTests.java
+++ kodilla-spring-web/src/test/java/com/kodilla/kodillaspringweb/KodillaSpringWebApplicationTests.java
@@ -1,13 +1,13 @@
-package com.kodilla;
+package com.kodilla.kodillaspringweb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class KodillaCourseApplicationTests {
+class KodillaSpringWebApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
