d6f65a0516d4b5b98fa8a2f03f021bcf7e2a2185
Marcin Łukasik <marcin@eims.pl>
2020-11-08 18:52:01 +0100

    Update

--- kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -3,6 +3,7 @@ package com.kodilla.spring.forum;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -11,11 +12,15 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ForumUserTestSuite {
+    @Autowired
+    private ForumUser forumUser;
+
     @Test
     public void testGetUsername() {
         // Given
-        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring.forum");
-        ForumUser forumUser = context.getBean(ForumUser.class);
+        // ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        // ForumUser forumUser = context.getBean(ForumUser.class);
+        // Autowired to realizuje razem ze SBT
         // When
         String username = forumUser.getUsername();
         // Then
