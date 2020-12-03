63de6146b2dbcc5c1eb33bff1bc85422e6d76d56
eimspl <marcin@eims.pl>
2020-12-03 18:44:32 +0100

    Strategy

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/UserTestSuite.java
@@ -0,0 +1,44 @@
+package com.kodilla.patterns.strategy;
+
+import com.kodilla.patterns.strategy.social.*;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserTestSuite {
+    @Test
+    public void testDefaultInvestingStrategies() {
+        //Given
+        User olaf = new Millenials("Olaf Kowlaski");
+        User brajan = new YGeneration("Brajan Nowak");
+        User mahomet = new ZGeneration("Mahomet Iksiński");
+
+        //When
+        String olafShouldShare = olaf.whereShare();
+        System.out.println("Olaf: " + olafShouldShare);
+        String brajanShouldShare = brajan.whereShare();
+        System.out.println("Brajan: " + brajanShouldShare);
+        String mahometShouldShare = mahomet.whereShare();
+        System.out.println("Mahomet: " + mahometShouldShare);
+
+        //Then
+        Assert.assertEquals("Millenials post on Facebook", olafShouldShare);
+        Assert.assertEquals("YGeneration post on Twitter", brajanShouldShare);
+        Assert.assertEquals("ZGeneration post on Snapchat", mahometShouldShare);
+    }
+
+    @Test
+    public void testIndividualInvestingStrategy() {
+        //Given
+        User albin = new Millenials("Albin Kolano");
+
+        //When
+        String albinShouldShare = albin.whereShare();
+        System.out.println("Albin: " + albinShouldShare);
+        albin.setDefaultSocial(new SnapchatPublisher());
+        albinShouldShare = albin.whereShare();
+        System.out.println("Albin after change preference: " + albinShouldShare);
+
+        //Then
+        Assert.assertEquals("ZGeneration post on Snapchat", albinShouldShare);
+    }
+}
